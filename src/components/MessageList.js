7c0731c42a0e08bb93499274bbbf406438af9018
developermsr <69053937+developermsr@users.noreply.github.com>
2023-11-04 22:18:47 +0100

    Update MessageList.js
    aa

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react';
-import './MessageList.css'; // Asume que tienes un archivo CSS para estilizar tu ChatBox
+import './MessageList.css';
+
 const MessageList = ({ messages }) => {
   const endOfMessagesRef = useRef(null);
 
@@ -7,16 +8,27 @@ const MessageList = ({ messages }) => {
     endOfMessagesRef.current?.scrollIntoView({ behavior: "smooth" });
   }, [messages]);
 
+  // Suponiendo que cada mensaje tiene un id único
+// ...
+
+// Filtro por contenido y usuario de mensajes consecutivos
+const uniqueMessages = messages.filter((msg, index, self) => {
+  if (index === 0) return true; // Siempre mantén el primer mensaje
+  const prevMsg = self[index - 1];
+  return msg.username !== prevMsg.username || msg.content !== prevMsg.content;
+});
+
+// ...
+
+
   return (
     <div className="message-list">
-      {/* Asegúrate de que los mensajes se muestren en el orden correcto */}
-      {messages.map((message, index) => (
-  <div key={index} className="message">
-    <div className="message-username">{message.username}</div>
-    <span className="message-content">{message.content}</span>
-  </div>
-))}
-
+      {uniqueMessages.map((message) => (
+        <div key={message.id} className="message"> {/* Cambiado 'key' para usar 'message.id' */}
+          <div className="message-username">{message.username}</div>
+          <span className="message-content">{message.content}</span>
+        </div>
+      ))}
       <div ref={endOfMessagesRef} />
     </div>
   );
