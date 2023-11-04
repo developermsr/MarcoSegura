c6b4c7d09e347606863eeeb253c2d84b33accccd
developermsr <69053937+developermsr@users.noreply.github.com>
2023-11-04 19:32:47 +0100

    Update ChatBox.js
    asdas

--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -3,15 +3,10 @@ import React, { useState, useEffect } from 'react';
 import MessageList from './MessageList'; // Asumiendo que tienes este componente
 import './ChatBox.css'; // Asume que tienes un archivo CSS para estilizar tu ChatBox
 import axios from 'axios';
-import io from 'socket.io-client';
 
 const ChatBox = ({ messages, setMessages, sendMessage }) => {
   const [newMessage, setNewMessage] = useState('');
-  const socket = io('https://marco-segura.vercel.app/api/message'); // Reemplaza con la URL de tu servidor WebSocket
-
-  // Maneja la recepción de nuevos mensajes desde el servidor WebSocket
  
-
   const handleSendMessage = () => {
     if (newMessage.trim()) {
       sendMessage(newMessage); // Usa directamente la función que se pasó como prop.
@@ -29,19 +24,12 @@ const ChatBox = ({ messages, setMessages, sendMessage }) => {
     }
   };
   useEffect(() => {
-    socket.on('message', (newMessage) => {
-      setMessages((prevMessages) => [...prevMessages, newMessage]);
-    });
-
-    return () => {
-      socket.disconnect();
-    },
     axios.get('https://marco-segura.vercel.app/api/messages')
       .then(response => {
         setMessages(response.data);
       })
       .catch(error => console.error('Error fetching messages:', error));
-  }, [setMessages,socket]); // Pasa setMessages para asegurarte de que no cambie y no provoque bucles infinitos.
+  }, [setMessages]); // Pasa setMessages para asegurarte de que no cambie y no provoque bucles infinitos.
   
 
   return (
